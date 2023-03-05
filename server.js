0c09b47d38ec8dbc58734af45f8524731bb80342
sadakhalil <s.halil@yahoo.com>
2023-03-05 21:34:38 +0100

    Author Routes added

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+    require('dotenv').config()
 }
 
 
 
-const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
-const indexRoute = require('./routes/index')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -23,6 +26,7 @@ const db = mongoose.connection
 db.on('error', error => {console.error(error);})
 db.once('open', () => console.log('Connected to the Mongoose'))
 
-app.use('/', indexRoute)
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
